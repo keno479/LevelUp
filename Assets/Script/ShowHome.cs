320ba5e8fb5d9216d1b831ad59e7729da859dd87
DESKTOP-R5FNI3G\keno4 <haraken47952@gmail.com>
2021-08-16 23:36:00 +0900

    Homeメニューを表示する地点を作った

---
+++ Assets/Script/ShowHome.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHome : MonoBehaviour
+{
+    public GameObject WindowHome;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            WindowHome.SetActive(true);
+        }
+    }
+}
