d3ac7a69dc9dc033187408059b6e55951877fbe1
DESKTOP-R5FNI3G\keno4 <haraken47952@gmail.com>
2021-09-02 21:15:52 +0900

    シーンを変更するとBGMが変更されるようにした

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using anogamelib;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public AudioClip[] clips;
+    private AudioSource audios;
+    private int SceneIndex;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        audios = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if (SceneIndex != SceneManager.GetActiveScene().buildIndex)
+        {
+            SceneIndex = SceneManager.GetActiveScene().buildIndex;
+            audios.clip = clips[SceneIndex];
+            audios.Play();
+        }
+    }
+}
